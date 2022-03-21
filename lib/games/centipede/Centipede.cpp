3e2b4375b383be9027dbd86a91c11f6c806094ca
MarvinFLm <marvin.flamand@epitech.eu>
2022-03-21 09:52:37 +0100

    create one snake

--- lib/games/centipede/Centipede.cpp
+++ lib/games/centipede/Centipede.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
+/*
+** EPITECH PROJECT, 2022
+** B-OOP-400-LYN-4-1-arcade-marvin.flamand
+** File description:
+** main
+*/
 
-void centipede()
+#include "Centipede.hpp"
+
+arc::games::Centipede::Centipede()
 {
-    std::cout << "Nibbler" << std::endl;
+}
+
+arc::games::Centipede::~Centipede()
+{
+
+}
+
+arc::games::Snake::Snake()
+{
+    int i = 0;
+    int size = 10;
+    int ax_x = 0;
+
+    for (; i < size - 1; i++, ax_x += 25) {
+            snake.push_back(arc::games::SnakeCells{});
+            snake[i].x = ax_x;
+            snake[i].y = 0;
+            snake[i].width = 25;
+            snake[i].height = 25;
+        if (i == 0)
+            snake[i].type = snake[i].HEAD;
+        else
+            snake[i].type = snake[i].BODY;
+    }
+}
+
+arc::games::Snake::~Snake()
+{
+
 }
--- lib/games/centipede/includes/Centipede.hpp
+++ lib/games/centipede/includes/Centipede.hpp
@@ -1,3 +1,85 @@
 #pragma once
 
-void centipede();
+#include <IGameModule.hpp>
+#include <Object.hpp>
+
+#include <iostream>
+
+namespace arc::games {
+
+    class Centipede : public arc::games::IGameModule {
+        public:
+            /**
+             * @brief Construct a new Centipede object
+             *
+             */
+            Centipede();
+
+            /**
+             * @brief Destroy the Centipede object
+             *
+             */
+            ~Centipede();
+
+        private:
+            std::vector<arc::Object> entities;
+
+    };
+
+
+    struct SnakeCells
+    {
+        /**
+         * @brief Define what is cell
+         *
+         */
+        enum Type { HEAD, BODY };
+
+        /**
+         * @brief Origin of cell on x axis
+         *
+         */
+        int x;
+        /**
+         * @brief Origin of cell on y axis
+         *
+         */
+        int y;
+        /**
+         * @brief Size of cell on x axis
+         *
+         */
+        int width;
+        /**
+         * @brief Size of cell on y axis
+         *
+         */
+        int height;
+        Type type;
+    };
+
+    class Snake {
+        public:
+            /**
+             * @brief Construct a new Snake object
+             *
+             */
+            Snake();
+            /**
+             * @brief Destroy the Snake object
+             *
+             */
+            ~Snake();
+        private:
+            /**
+             * @brief Array of cell = Snake
+             *
+             */
+            std::vector<arc::games::SnakeCells> snake;
+            /**
+             * @brief Size of snake
+             *
+             */
+            int size;
+    };
+};
