a7297c84a93861befaefb3cac08fe6ff0bf18ec2
Lucas-COX <lucas.cox@epitech.eu>
2022-03-22 14:06:01 +0100

    feat(games): add AGame class

--- lib/games/menu/MenuGame.cpp
+++ lib/games/menu/MenuGame.cpp
@@ -3,12 +3,11 @@
 #include <iostream>
 
 arc::games::MenuGame::MenuGame()
-    : m_props({"", "", ""})
+    : AGame(0)
+    , m_props({"", "", ""})
     , m_isStarting(false)
-    , m_isRunning(true)
 {
 }
-
 
 arc::games::MenuGame::~MenuGame() = default;
 
@@ -18,24 +17,9 @@ void arc::games::MenuGame::useEvent(arc::Events event)
         this->m_isRunning = false;
 }
 
-const std::vector<std::shared_ptr<arc::Object>> arc::games::MenuGame::getObjects() const
-{
-    return {};
-}
-
-bool arc::games::MenuGame::isRunning() const
-{
-    return this->m_isRunning;
-}
-
 void arc::games::MenuGame::update()
 {
     //todo update game state
-}
-
-const std::string arc::games::MenuGame::getUsername() const
-{
-    return this->m_props.username;
 }
 
 const arc::games::MenuProprieties arc::games::MenuGame::getProps() const
