edc7f4e8f3061a1c1649e6fbffe0cb1cd68599f7
Lucas-COX <lucas.cox@epitech.eu>
2022-03-23 17:58:14 +0100

    feat(arcade): huge Object refactoring

---
+++ src/includes/Vector.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace arc {
+
+    /**
+     * @brief Int vector
+     *
+     */
+    struct Vector {
+        int x;
+        int y;
+    }; /* struct Vector */
+
+} /* namespace arc */
