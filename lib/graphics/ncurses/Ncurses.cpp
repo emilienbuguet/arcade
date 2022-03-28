9214579af5e3097bd4174f09e3c7af9fdff82626
Buguet <emilien.buguet@epitech.eu>
2022-03-28 08:46:00 +0200

    feat(ncurses): add ncurses display

--- lib/graphics/ncurses/Ncurses.cpp
+++ lib/graphics/ncurses/Ncurses.cpp
@@ -1,48 +1,9 @@
-#include <iostream>
 #include "Ncurses.hpp"
 
-Ncurses::Ncurses()
-{
-    initscr();
-    nodelay(stdscr, TRUE);
-}
+extern "C" {
 
-Ncurses::~Ncurses()
+arc::display::NcursesDisplay *arc::display::createInstance()
 {
-    endwin();
+    return (new arc::display::NcursesDisplay {});
 }
-
-void Ncurses::drawObjects(std::vector<std::shared_ptr<arc::Object>> objs)
-{
-    clear();
-    std::string chr;
-    std::ifstream asset;
-    for (std::shared_ptr<arc::Object> i : objs) {
-        arc::Object obj = *i.get();
-        if (obj.type == obj.TEXT)
-            mvprintw(obj.x, obj.y, obj.value.c_str());
-        else {
-            asset.open("src/assets/ncurses/" + obj.value);
-            getline(asset, chr);
-            mvprintw(obj.x, obj.y, chr.c_str());
-        }
-    }
-}
-
-const arc::Events Ncurses::getEvent() const
-{
-    char in = getch();
-    if (in == ERR)
-        return arc::Events::None;
-    if (in == KEY_LEFT)
-        return arc::Events::KeyLeft;
-    if (in == KEY_DOWN)
-        return arc::Events::KeyDown;
-    if (in == KEY_RIGHT)
-        return arc::Events::KeyRight;
-    if (in == KEY_UP)
-        return arc::Events::KeyUp;
-    if (in == ' ')
-        return arc::Events::KeySpace;
-    return arc::Events::None;
-}
+};
