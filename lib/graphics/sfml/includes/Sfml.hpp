5c0e438e76d90892aaccdc14f9d6cadf31ca6e73
Lucas-COX <lucas.cox@epitech.eu>
2022-04-07 14:40:59 +0200

    feat(sfml): add functional sfml

--- lib/graphics/sfml/includes/Sfml.hpp
+++ lib/graphics/sfml/includes/Sfml.hpp
@@ -1,3 +1,18 @@
+#include <SfmlDisplay.hpp>
+#include <exception>
+#include <string>
+
 #pragma once
 
-void sfml();
+namespace arc::display {
+
+    extern "C" {
+        /**
+         * @brief Create a new instance of the SfmlDisplay
+         *
+         * @return SfmlDisplay* - A pointer to the new instance
+         */
+        SfmlDisplay *createInstance();
+    } /* extern "C" */
+
+}; /* namespace arc::display */
