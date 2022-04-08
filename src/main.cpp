e314acdee3eb16f3e4e22c9ce5703187bb0acd17
Buguet <emilien.buguet@epitech.eu>
2022-04-08 14:02:58 +0200

    feat(main): usage

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        std::cerr << "arcade: Wrong amount of arguments." << std::endl;
+        std::cerr << "USAGE:" << std::endl;
+        std::cerr << "\t\t ./arcade display_lib" << std::endl;
         return 84;
     }
     try {
